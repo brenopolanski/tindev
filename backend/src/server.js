d3fe09e36fceced5380dac052d2a627f42e1ec8e
brenopolanski <breno.polanski@gmail.com>
2020-01-03 21:59:56 -0300

    Final code for the Tindev project

---
+++ backend/src/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+  const { user } = socket.handshake.query;
+
+  connectedUsers[user] = socket.id;
+});
+
+mongoose.connect('mongodb+srv://admin:srqVuiHKeOlywC26@cluster0-dddme.mongodb.net/tindev?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
